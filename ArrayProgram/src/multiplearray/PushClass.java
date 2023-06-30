1d59dbdfde6e15d46c2ba7466db9a0362ffccb2a
Bhartiverma1989 <104622073+Bhartiverma1989@users.noreply.github.com>
2023-06-30 12:40:46 +0530

    class contains steps to push the code

---
+++ ArrayProgram/src/multiplearray/PushClass.java
@@ -0,0 +1,23 @@
+package multiplearray;
+
+public class PushClass {
+	
+	public static void main(String[] args) {
+		
+		// way to push the code
+		//steap 1: create class
+		
+		//step2 : add some code
+		
+		//step 3 : click on project-->team--->commit-->
+		
+		//step 4: click on plus icon
+		
+		//step 5 : add cpmmit
+		
+		//step 6: click on push head
+		
+		System.out.println("These are the steps for push the code");
+	}
+
+}
